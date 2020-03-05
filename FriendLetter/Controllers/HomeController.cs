c0542394470c9dc8c4b1844aaa4bf957dfc7e3c7
Brooke Kullberg <brookekullberg@gmail.com>
2020-03-05 12:22:50 -0800

    add new Postcard() route to HomeController

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -12,7 +12,8 @@ namespace FriendLetter.Controllers
     public string Goodbye() { return "Goodbye friend."; }
 
     [Route("/")]
-    public ActionResult Letter() { 
+    public ActionResult Letter() 
+    { 
       LetterVariable myLetterVariable = new LetterVariable();
       myLetterVariable.Recipient = "Lina";
       myLetterVariable.Sender = "Jasmine";
@@ -21,5 +22,14 @@ namespace FriendLetter.Controllers
 
     [Route("/form")]
     public ActionResult Form() { return View(); }
+
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
+      return View(myLetterVariable);
+    }
   }
 }
